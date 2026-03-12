43d59cd532cfa1f21d9f72b375b3bd6068566b2e
Tom Frumy <tom.frumy170@gmail.com>
2026-03-12 15:59:35 +0100

    Fix: deployement

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Importation des fichiers de traduction
 import enTranslations from './locales/en.json';
 import frTranslations from './locales/fr.json';
 
